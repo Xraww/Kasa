3bc47a0ebaf41c7812ac971d4eaff03715dd9ff9
Xraww <xrw3008@gmail.com>
2024-06-19 03:21:29 +0200

    Error page on bad id & nav links underline on active section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 import "../styles/Header.scss";
 import logo from "../assets/logo.png";
 
 export default function Header() {
+    const [activeSection, setActiveSection] = useState("home");
+
+    useEffect(() => {
+        const lastActiveSection = localStorage.getItem("activeSection");
+
+        if (lastActiveSection) {
+            setActiveSection(lastActiveSection);
+        }
+    }, []);
+  
+    const handleSetActive = (section) => {
+        setActiveSection(section);
+        localStorage.setItem("activeSection", section);
+    };
+
   return (
     <header>
         <div className="navbar">
             <img src={logo} alt="Logo du site"/>
             
             <div className="navbar-links">
-                <Link to="/home" className="links">Accueil</Link>
-                <Link to="/about" className="links">A Propos</Link>
+                <Link to="/home" className={`links ${activeSection === "home" ? "active" : ""}`} onClick={() => handleSetActive("home")}>Accueil</Link>
+                <Link to="/about" className={`links ${activeSection === "about" ? "active" : ""}`} onClick={() => handleSetActive("about")}>A Propos</Link>
             </div>
         </div>
     </header>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
@@ -16,7 +16,8 @@ root.render(
             <Route path="/home" element={<Home/>}/>
             <Route path="/home/logements/:logementId" element={<LogementDetails />} />
             <Route path="/about" element={<About/>}/>
-            <Route path="*" element={<Error />} />
+            <Route path="/error" element={<Error />} />
+            <Route path="*" element={<Navigate to="/error" />} />
         </Routes>
     </Router>
   </React.StrictMode>
